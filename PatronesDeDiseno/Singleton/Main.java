c6d8c2e386eb4f134a4f92a06d33c0f53dd2d200
Diego2021-cibercom <dgarciai@redcibercom.com.mx>
2023-11-06 21:16:19 -0600

    instancia singleton

---
+++ PatronesDeDiseno/Singleton/Main.java
@@ -0,0 +1,7 @@
+import Java.PatronesDeDiseno.Singleton.Singleton;
+
+public class Main {
+    public static void main(String [] args){
+        Singleton singleton=Singleton.getInstance();
+    }   
+}
