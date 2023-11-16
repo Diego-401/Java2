f7706748d632590925a53458131ebd0a0f8eae89
Diego2021-cibercom <dgarciai@redcibercom.com.mx>
2023-11-15 21:49:41 -0600

    update lista

--- EstructurasDeDatos/Lista/ListaDinamica.java
+++ EstructurasDeDatos/Lista/ListaDinamica.java
@@ -119,6 +119,98 @@ public class ListaDinamica<T> {
         return elementoNuevo;
     }
 
+    public boolean exists(T element){
+        boolean result=false;
+        if(!isEmpty()){
+            Nodo<T> nodo=primero;
+            while(nodo!=null && !result){
+                if(primero.getElemento()==element)
+                    result=true;
+                nodo=nodo.getSiguiente();
+            }
+        }
+        return result; 
+    }
+
+    public int indexOf(T element){
+        int result=-1;
+        if(isEmpty()){
+            return result;
+        }else{
+            Nodo<T> nodo=primero;
+            while(nodo!=null){
+                result++;
+                if(primero.getElemento()==element)
+                    return result;
+                nodo=nodo.getSiguiente();
+            }
+        }
+        return result; 
+    }
+
+    public T removeFirst(){
+        T element=null;
+        Nodo<T> nodoRemove=null;
+        if(!isEmpty()){
+            nodoRemove=primero;
+            element=nodoRemove.getElemento();
+            primero=null;
+            if(size()>1){
+                primero=nodoRemove.getSiguiente();
+                primero.setAnterior(null);
+            }else if(size()==1){
+                ultimo=null;
+            }
+            numeroElementos--;
+        }
+        return element;
+    }
+
+    public T removeLast(){
+        T element=null;
+        Nodo<T> nodoRemove=null;
+        if(!isEmpty()){
+            nodoRemove=ultimo;
+            element=nodoRemove.getElemento();
+            ultimo=null;
+            if(size()>1){
+                ultimo=nodoRemove.getAnterior();
+                ultimo.setSiguiente(null);
+            }else if(size()==1){
+                primero=null;
+            }
+            numeroElementos--;
+        }
+        return element;
+    }
+
+    public T remove(int index){
+        T element =null;
+        Nodo <T> nodoAux=getNodo(index);
+        if(nodoAux!=null){
+            if(size()==1){
+                element=nodoAux.getElemento();
+                primero=null;
+                ultimo=null;
+            }else{
+                Nodo<T> nodoSiguiente=nodoAux.getSiguiente();
+                Nodo<T> nodoAnterior=nodoAux.getAnterior();
+                nodoSiguiente.setAnterior(nodoAnterior);
+                nodoAnterior.setSiguiente(nodoSiguiente);
+            }
+            nodoAux=null;
+            numeroElementos--;
+        }
+        return element;
+    }
+
+    public T modify(T element, int index){
+        Nodo <T> nodoAux=getNodo(index);
+        nodoAux.setElemento(element);
+        return nodoAux.getElemento();
+    }
+    
+
     @Override
     public String toString(){
         String resultado="";
